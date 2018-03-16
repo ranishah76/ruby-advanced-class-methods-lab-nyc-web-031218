b2d66d26c0079379a520fed451b8f406cc41af79
Rani Shah <rani76@gmail.com>
2018-03-16 19:11:31 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -48,10 +48,9 @@ class Song
 
  def self.new_from_filename(filename)
    song_and_artist = filename.split(" - ")
-   name = song_and_artist[1].gsub!(".mp3", "")
+   name = song_and_artist[1].gsub!(".mp3", " ")
    binding.pry
    artist_name = song_and_artist[0]
-
    song = self.new
    song.name = name
    binding.pry
